15864cdef770b91cb8cff8d041bc144a889f972c
weiyalin <727299708@qq.com>
2018-08-15 14:10:20 +0800

    first commit

---
+++ src/components/test.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link to="/login">login</router-link>
+        <router-link to="/">HelloWorld</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test"
+    }
+</script>
+
+<style scoped>
+
+</style>
