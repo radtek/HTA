58ce800ee5828ac3b135c4006e2a6ff32f93d611
weiyalin <727299708@qq.com>
2018-09-19 22:41:44 +0800

    添加行政检查页面

--- src/components/administration/Check.vue
+++ src/components/administration/Check.vue
@@ -1,10 +1,35 @@
 <template>
-    
+    <div>
+        <div style="width: 100%;height: 40px;"><myHeard back="true" title="日常检查"></myHeard></div>
+
+        <!--<div>-->
+            <!--<mt-cell title="标题文字" value="说明文字"></mt-cell>-->
+            <!--<mt-field label="验证码" v-model="captcha">-->
+                <!--<img src="../assets/100x100.png" height="45px" width="100px">-->
+            <!--</mt-field>-->
+        <!--</div>-->
+    </div>
 </template>
 
 <script>
+    import myHeard from "../customComponent/myHeard"
     export default {
-        name: "check"
+        name: "check",
+        components:{
+            myHeard
+        },
+        data() {
+            return {
+                type : 1,
+            };
+        },
+        methods: {
+
+        },
+        mounted() {
+            this.type = this.$route.params.type;
+            this.getData();
+        }
     }
 </script>
 
