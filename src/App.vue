6717c320452330bed219a3c3aca0d828b1bb5714
weiyalin <727299708@qq.com>
2018-09-21 11:49:17 +0800

    '添加element主题'

--- src/App.vue
+++ src/App.vue
@@ -57,4 +57,65 @@
         text-align: left;
         font-family: normal;
     }
+
+    /*修改mint-ui默认样式*/
+    .mint-header {
+        background-color: rgb(110,193,191);;
+    }
+    .mint-button:not(.is-disabled):active::after {
+        opacity: .2;
+    }
+    .mint-button--primary {
+        background-color: rgb(110,193,191);
+    }
+    .mint-button--primary.is-plain {
+        border: 1px solid rgb(110,193,191);
+        color: rgb(110,193,191);
+    }
+    .mint-badge.is-primary {
+        background-color: rgb(110,193,191);
+    }
+    .mint-switch-input:checked + .mint-switch-core {
+        border-color: rgb(110,193,191);
+        background-color: rgb(110,193,191);
+    }
+    .mint-navbar .mint-tab-item.is-selected {
+        border-bottom: 3px solid rgb(110,193,191);
+        color: rgb(110,193,191);
+    }
+    .mint-tabbar > .mint-tab-item.is-selected {
+        color: rgb(110,193,191);
+    }
+    .mint-searchbar-cancel {
+        color: rgb(110,193,191);
+    }
+    .mint-checkbox-input:checked + .mint-checkbox-core {
+        background-color: rgb(110,193,191);
+        border-color: rgb(110,193,191);
+    }
+    .mint-radio-input:checked + .mint-radio-core {
+        background-color: rgb(110,193,191);
+        border-color: rgb(110,193,191);
+    }
+    .mt-range-progress {
+        background-color: rgb(110,193,191);
+    }
+    .mt-progress-progress {
+        background-color: rgb(110,193,191);
+    }
+    .mint-msgbox-confirm {
+        color: rgb(110,193,191);
+    }
+    .mint-msgbox-confirm:active {
+        color: rgb(110,193,191);
+    }
+    .mint-datetime-action {
+        color: rgb(110,193,191);
+    }
+
+    /*element-ui*/
+    .select .el-input-group__append{
+        color: white;
+        background-color: rgb(94,186,185);
+    }
 </style>
--- src/components/administration/Check.vue
+++ src/components/administration/Check.vue
@@ -3,7 +3,7 @@
         <div style="width: 100%;height: 40px;"><myHeard back="true" title="日常检查"></myHeard></div>
 
         <div class="bmt">
-            <myField label="执法对象" placeholder="请选择" v-model="form.obj" left-img="true" red-point="true"></myField>
+            <a @click="popupVisible = true"><myField label="执法对象" placeholder="请选择" v-model="form.obj" left-img="true" red-point="true"></myField></a>
             <myField label="检查人" placeholder="请输入" v-model="form.checkMan" red-point="true"></myField>
             <myField label="协办人员" placeholder="请选择" v-model="form.jointly" left-img="true" red-point="true"></myField>
             <myField label="办理时间" placeholder="请选择" v-model="form.time" left-img="true" red-point="true"></myField>
@@ -11,24 +11,60 @@
         </div>
 
         <div class="bmt">
-            <myField label="检查内容" red-point="true" readonly="true"></myField>
+            <myField label="检查项目" red-point="true" :readonly="true"></myField>
+            <a @click="allClick()"><mt-checklist v-model="all" :options="['全选']"></mt-checklist></a>
+            <mt-checklist v-model="checkList" :options="checkOptions"></mt-checklist>
+        </div>
 
+        <div class="select">
+            <mt-popup
+                    v-model="popupVisible"
+                    position="right">
+                <div style="height:100%;overflow:scroll;">
+                    <div style="width: 100%;height: 40px;">
+                        <mt-header fixed title="执法对象">
+                            <router-link to="" slot="left">
+                                <mt-button class="myColor" icon="back" @click="popupVisible = false"></mt-button>
+                            </router-link>
+                        </mt-header>
+                    </div>
+
+                    <div class="selectBut">
+                        <el-input placeholder="请输入内容" v-model="selectValue" class="input-with-select" size="medium">
+                            <el-button slot="append" icon="el-icon-search"></el-button>
+                        </el-input>
+                    </div>
+
+                    <div class="mt">
+
+                    </div>
+                </div>
+
+            </mt-popup>
         </div>
     </div>
 </template>
 
 <script>
     import myHeard from "../customComponent/myHeard";
-    import myField from  "../customComponent/myField"
+    import myField from  "../customComponent/myField";
+    import {Toast, Indicator} from 'mint-ui';
     export default {
         name: "check",
         components:{
             myHeard,
-            myField
+            myField,
+            Toast,
+            Indicator
         },
         data() {
             return {
                 type : 1,
+                all  : [],
+                popupVisible: false,
+                checkOptions: [],
+                checkList   : [],
+                selectValue : '',
                 form : {
                     obj     :'',
                     checkMan:'',
@@ -39,14 +75,56 @@
             };
         },
         methods: {
-
+            allClick:function () {
+                //this.all.length是点击之前的值
+                this.all.length === 0 ? this.checkList = this.checkOptions : this.checkList = [];
+            },
+            getCheckList:function () {
+                Indicator.open();
+                let self = this;
+                $.get(getUrl('sf_zhzf/msys/inspnotes/inspitems'), {
+                    inspSpecial: self.type
+                }, function (data, status) {
+                    Indicator.close();
+                    if (data.statusCode == 200) {
+                        data.list.forEach(function (value,index) {
+                            self.checkOptions.push(value.inspName);
+                        })
+                    } else if (data.statusCode == 310) {
+                        window.location.href = "login.html";
+                    } else {
+                        Toast(data.message);
+                    }
+                }, 'json');
+            }
         },
         mounted() {
             this.type = this.$route.params.type;
+            this.getCheckList();
         }
     }
 </script>
 
 <style scoped>
-
+    .select{
+        width: 100%;
+        height: 100%;
+    }
+    .select .mint-popup{
+        width: 100%;
+        height: 100%;
+        background-color: rgb(248,249,249);
+    }
+    .select .mint-header{
+        background-color: white;
+        color: rgb(0,149,147);
+        font-size: 14px !important;
+    }
+    .myColor{
+        color: rgb(0,149,147);
+    }
+    .selectBut{
+        padding: 10px;
+        background-color: white;
+    }
 </style>
