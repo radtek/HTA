b40f4ec35c58be7938167949d7cdcc6c02aa3384
weiyalin <727299708@qq.com>
2018-09-21 22:02:07 +0800

    '封装提交按钮'

--- src/components/administration/Check.vue
+++ src/components/administration/Check.vue
@@ -3,10 +3,14 @@
         <div style="width: 100%;height: 40px;"><myHeard back="true" title="日常检查"></myHeard></div>
 
         <div class="bmt">
-            <a @click="popupVisible = true"><myField label="执法对象" placeholder="请选择" v-model="form.obj" left-img="true" red-point="true"></myField></a>
+            <a @click="popupVisible = true;getObj();">
+                <myField label="执法对象" placeholder="请选择" v-model="form.obj" left-img="true" red-point="true"></myField>
+            </a>
             <myField label="检查人" placeholder="请输入" v-model="form.checkMan" red-point="true"></myField>
-            <myField label="协办人员" placeholder="请选择" v-model="form.jointly" left-img="true" red-point="true"></myField>
-            <myField label="办理时间" placeholder="请选择" v-model="form.time" left-img="true" red-point="true"></myField>
+            <myField label="协办人员" placeholder="请输入" v-model="form.jointly" red-point="true"></myField>
+            <a @click="openPicker">
+                <myField label="办理时间" placeholder="请选择" v-model="form.time" left-img="true" red-point="true"></myField>
+            </a>
             <myField label="检查内容" placeholder="请输入" v-model="form.content" red-point="true" type="textarea"></myField>
         </div>
 
@@ -15,6 +19,8 @@
             <a @click="allClick()"><mt-checklist v-model="all" :options="['全选']"></mt-checklist></a>
             <mt-checklist v-model="checkList" :options="checkOptions"></mt-checklist>
         </div>
+
+        <myFlaxSub title="提交" @click="sub"></myFlaxSub>
 
         <div class="select">
             <mt-popup
@@ -31,29 +37,42 @@
 
                     <div class="selectBut">
                         <el-input placeholder="请输入内容" v-model="selectValue" class="input-with-select" size="medium">
-                            <el-button slot="append" icon="el-icon-search"></el-button>
+                            <el-button slot="append" icon="el-icon-search" @click="getObj"></el-button>
                         </el-input>
                     </div>
 
                     <div class="mt">
-
+                        <a v-for="item in objList" :key="item.id" @click="selectedObj(item.objName)">
+                            <div class="myObj">
+                                <p>{{ item.objName }}</p>
+                            </div>
+                        </a>
                     </div>
                 </div>
-
             </mt-popup>
         </div>
+
+        <mt-datetime-picker
+                v-model="dataValue"
+                ref="picker"
+                type="date"
+                @confirm="handleConfirm"
+        >
+        </mt-datetime-picker>
     </div>
 </template>
 
 <script>
     import myHeard from "../customComponent/myHeard";
     import myField from  "../customComponent/myField";
+    import myFlaxSub from  "../customComponent/myFlaxSub";
     import {Toast, Indicator} from 'mint-ui';
     export default {
         name: "check",
         components:{
             myHeard,
             myField,
+            myFlaxSub,
             Toast,
             Indicator
         },
@@ -65,6 +84,8 @@
                 checkOptions: [],
                 checkList   : [],
                 selectValue : '',
+                objList     : [],
+                dataValue  :new Date(),
                 form : {
                     obj     :'',
                     checkMan:'',
@@ -96,12 +117,56 @@
                         Toast(data.message);
                     }
                 }, 'json');
+            },
+            getObj:function () {
+                Indicator.open();
+                let self = this;
+                $.get(getUrl('sf_zhzf/msys/enterprise/querybyname2'), {
+                    inspSpecial: self.type,
+                    objName    : self.selectValue
+                }, function (data, status) {
+                    Indicator.close();
+                    console.log(data);
+                    if (data.statusCode == 200) {
+                        self.objList = data.list;
+                    } else if (data.statusCode == 310) {
+                        window.location.href = "login.html";
+                    } else {
+                        Toast(data.message);
+                    }
+                }, 'json');
+            },
+            selectedObj:function (obj) {
+                this.form.obj = obj;
+                this.popupVisible = false;
+            },
+            openPicker:function () {
+                this.$refs.picker.open();
+            },
+            handleConfirm:function (value) {
+                let dateVal = new Date(value);
+                this.form.time = this.myFormat(dateVal);
+            },
+            myFormat:function (dateVal) {
+
+                let year    = dateVal.getFullYear();
+                let month   = dateVal.getMonth() + 1;
+                let date    = dateVal.getDate();
+
+                month   < 10 && (month   = '0'+month);
+                date    < 10 && (date    = '0'+date);
+
+                return year + '-' + month + '-' + date;
+            },
+            sub:function () {
+                console.log(555);
             }
         },
         mounted() {
             this.type = this.$route.params.type;
             this.getCheckList();
-        }
+            this.form.time = this.myFormat(new Date());
+        },
     }
 </script>
 
@@ -118,7 +183,7 @@
     .select .mint-header{
         background-color: white;
         color: rgb(0,149,147);
-        font-size: 14px !important;
+        font-size: 16px !important;
     }
     .myColor{
         color: rgb(0,149,147);
@@ -126,5 +191,14 @@
     .selectBut{
         padding: 10px;
         background-color: white;
+    }
+    .myObj{
+        margin-bottom: 1px;
+        background-color: white;
+        height: 30px;
+        line-height: 30px;
+        padding: 2px 15px;
+        font-size: 14px;
+        color: rgb(96,96,96);
     }
 </style>
