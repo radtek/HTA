3408a4622f6b07502c3bd2dab8b1ba79b74bba3f
孙记风 <2181550591@qq.com>
2018-09-22 16:18:03 +0800

    上传

--- src/components/task/TaskDealWith.vue
+++ src/components/task/TaskDealWith.vue
@@ -16,16 +16,29 @@
 
 
 
-        <el-upload
-                action="https://jsonplaceholder.typicode.com/posts/"
-                list-type="picture-card"
-                :on-preview="handlePictureCardPreview"
-                :on-remove="handleRemove">
-            <i class="el-icon-plus"></i>
-        </el-upload>
-        <el-dialog :visible.sync="dialogVisible">
-            <img width="100%" :src="dialogImageUrl" alt="">
-        </el-dialog>
+        <div class="weui_cells weui_cells_form">
+            <div class="weui_cell">
+                <div class="weui_uploader_hd weui_cell" id="picture_content">
+                    <div class="font-position">照片</div>
+                    <div class="add-img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="add-img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="add-img">
+                        <img src="" alt="">
+                    </div>
+                    <div class="add-img">
+                        <img src="" alt="">
+                    </div>
+
+                    <div id="add" class="weui-uploader-input-wrp" @click= "captureImage()">
+                        <input type="file" id="file_input" multiple/>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
@@ -61,7 +74,6 @@
                 </div>
             </mt-popup>
         </div>
-
         <mt-datetime-picker
                 v-model="dataValue"
                 ref="picker"
@@ -89,19 +101,20 @@
         data() {
             return {
                 type : 1,
-                popupVisible: false,
-                selectValue : '',
-                objList     : [],
-                dataValue  :new Date(),
+                popupVisible  : false,
+                selectValue   : '',
+                objList       : [],
+                dataArr       : [],
+                dataValue     :new Date(),
                 form : {
-                    obj     :'',
-                    checkMan:'',
-                    jointly :'',
-                    time    :'2018-08-23 14:22',
-                    content :'',
+                    obj       :'',
+                    checkMan  :'',
+                    jointly   :'',
+                    time      :'2018-08-23 14:22',
+                    content   :'',
                 },
                 dialogImageUrl: '',
-                dialogVisible: false
+                dialogVisible : false
             };
         },
         methods: {
@@ -156,7 +169,7 @@
                 this.$refs.picker.open();
             },
             handleConfirm:function (value) {
-                let dateVal = new Date(value);
+                let dateVal    = new Date(value);
                 this.form.time = this.myFormat(dateVal);
             },
             myFormat:function (dateVal) {
@@ -172,6 +185,45 @@
             },
             sub:function () {
                 console.log(555);
+            },
+            captureImage: function () {
+                let self = this;
+                let cmr  = plus.camera.getCamera();
+                let res  = cmr.supportedImageResolutions[0];
+                let fmt  = cmr.supportedImageFormats[0];
+                console.log("Resolution: "+res+", Format: "+fmt);
+                cmr.captureImage( function( path ){
+                        console.log( "Capture image success: " + path );
+                        let image      = new Image();
+                        image.src      = path;
+                        image.onload   = function(){
+                            let base64 = self.getBase64Image(image);
+                            result     = '<div class="delete">delete</div><div class="result"><img src="'+base64+'" alt=""/></div>';
+                            let div    = document.createElement('div');
+                            div.innerHTML    = result;
+                            div['className'] = 'add-img';
+                            document.getElementById("picture_content").insertBefore(div,document.getElementById("add"));  　　//插入dom树
+                            console.log(base64);
+                            div.onclick = function(){
+                                this.remove();                  // 在页面中删除该图片元素
+                            }
+                        }
+                    },
+                    function( error ) {
+                        console.log( "Capture image failed: " + error.message );
+                    },
+                    {resolution:res,format:fmt}
+                );
+            },
+            getBase64Image(img) {
+                var canvas      = document.createElement("canvas");
+                canvas.width    = img.width;
+                canvas.height   = img.height;
+                var ctx         = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0, img.width, img.height);
+                var ext         = img.src.substring(img.src.lastIndexOf(".")+1).toLowerCase();
+                var dataURL     = canvas.toDataURL("image/"+ext);
+                return dataURL;
             }
         },
         mounted() {
@@ -212,5 +264,102 @@
         padding: 2px 15px;
         font-size: 14px;
         color: rgb(96,96,96);
+    }
+
+    .weui_cells {
+        margin-top: 10px;
+        background-color: #FFFFFF;
+        line-height: 1.41176471;
+        font-size: 17px;
+        overflow: hidden;
+        position: relative;
+        padding-bottom: 19px;
+    }
+    .weui_cell {
+        padding: 10px 15px;
+        align-items: center;
+    }
+    .weui_uploader_hd {
+        padding-top: 0;
+        padding-right: 0;
+        padding-left: 0;
+        padding: 10px 15px;
+        position: relative;
+    }
+
+    .weui-uploader-input-wrp {
+        float: left;
+        position: relative;
+        margin: 9px;
+        width: 77px;
+        height: 77px;
+        border: 1px solid #D9D9D9;
+        background-color: rgb(245,245,245);
+    }
+    #file_input {
+        display: none;
+        font: 100% tahoma, \5b8b\4f53, arial;
+        vertical-align: baseline;
+        border-radius: 0;
+        background-color: transparent;
+        -webkit-appearance: none;
+    }
+    .weui-uploader-input-wrp:before {
+        width: 2px;
+        height: 39.5px;
+    }
+    .weui-uploader-input-wrp:after {
+        width: 39.5px;
+        height: 2px;
+    }
+    .weui-uploader-input-wrp:before, .weui-uploader-input-wrp:after {
+        content: " ";
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        -webkit-transform: translate(-50%, -50%);
+        transform: translate(-50%, -50%);
+        background-color: #D9D9D9;
+    }
+    .font-position{
+        margin-right:20px;
+        background-size: cover;
+        margin-bottom: 6px;
+    }
+    .add-img{
+        float: left;
+        margin: 9px;
+        width: 77px;
+        height: 77px;
+        border: 1px solid #D9D9D9;
+    }
+    .add-img img{
+        width: 100%;
+        height: 100%;
+    }
+    .delete{
+        width: 69px;
+        height: 69px;
+        position: absolute;
+        text-align: center;
+        line-height: 80px;
+        z-index: 10;
+        font-size: 23px;
+        background-color: rgba(255,255,255,0.8);
+        color: #777;
+        opacity: 0;
+        transition-duration: 0.7s;
+        -webkit-transition-duration: 0.7s;
+    }
+
+    .delete:hover{
+        cursor: pointer;
+        opacity: 1;
+    }
+    .result{
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        box-sizing: border-box;
     }
 </style>
