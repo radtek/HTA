94c4cc066b2de854c1c18b81bcdec9d5e6f403a2
weiyalin <727299708@qq.com>
2018-10-12 20:50:32 +0800

    '旧版修改域名'

--- static/config/config.js
+++ static/config/config.js
@@ -9,7 +9,7 @@ var inspType = 7;
  * @returns {string}
  */
 function getUrl(url) {
-    return 'http://www.iotshifang.com/' + url + '?jsonp=?';
+    return 'http://61.54.107.162:8089/' + url + '?jsonp=?';
 }
 
 /**
